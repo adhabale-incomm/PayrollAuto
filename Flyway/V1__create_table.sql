f6383cb80f3dd7bf473a571b3c20a8c2b5392fba
adhabale-incomm <adhabale@InComm.com>
2021-04-05 13:27:32 -0400

    Update V1__create_table.sql

--- Flyway/V1__create_table.sql
+++ Flyway/V1__create_table.sql
@@ -3,5 +3,5 @@ Create table f_test1(id number(8),name varchar(20));
 ALTER TABLE f_test1 
 ADD updatedate DATE ;
 
-ALTER TABLE f_test1 
-ADD lastname varchar(50) ;
+--ALTER TABLE f_test1 
+--ADD lastname varchar(50) ;
