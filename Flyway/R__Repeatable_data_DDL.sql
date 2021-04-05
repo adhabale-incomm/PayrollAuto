8830dc2967d2ead6dee594b316705c98e0781a8c
adhabale-incomm <adhabale@InComm.com>
2021-04-05 13:09:42 -0400

    update script

--- Flyway/R__Repeatable_data_DDL.sql
+++ Flyway/R__Repeatable_data_DDL.sql
@@ -1,4 +1,7 @@
 Create table test_re(id number(8),name varchar(50));
 
+--ALTER TABLE test_re 
+--ADD birth_date DATE ;
+
 ALTER TABLE test_re 
-ADD birth_date DATE ;
+ADD address varchar(50) ;
--- Flyway/V1__create_table.sql
+++ Flyway/V1__create_table.sql
@@ -1,3 +1,4 @@
 Create table f_test1(id number(8),name varchar(20));
 
---Insert into f_test1(id,name) values(1,'test1');
+ALTER TABLE f_test1 
+ADD birth_date DATE ;
