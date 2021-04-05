ac13cb03e2223fe0af6baeccfb47b5826ef0b2ec
adhabale-incomm <adhabale@InComm.com>
2021-04-05 13:06:38 -0400

    Update V1__create_table.sql

--- Flyway/V1__create_table.sql
+++ Flyway/V1__create_table.sql
@@ -1,3 +1,3 @@
-Create table f_test1(id number(8),name varchar(20))
+Create table f_test1(id number(8),name varchar(20));
 
-Insert into f_test1(id,name) values(1,'test1');
+--Insert into f_test1(id,name) values(1,'test1');
