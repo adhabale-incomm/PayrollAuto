f8528be95bb5c41ed07e7983df3f358d5db8113a
adhabale-incomm <adhabale@InComm.com>
2021-04-05 15:51:54 -0400

    Update R__Repeatable_data_DDL.sql

--- Flyway/R__Repeatable_data_DDL.sql
+++ Flyway/R__Repeatable_data_DDL.sql
@@ -1,4 +1,14 @@
---Create table test_re(id number(8),name varchar(50));
+declare n number(1):=0;
+
+Begin
+select count(*) into n from all_tables where table_name='TEST_RE';
+
+if (n=0) then
+
+Execute immediate 'Create table test_re(id number(8),name varchar(50))';
+ end if ;
+END;
+
 
 ALTER TABLE test_re 
 ADD birth_date DATE ;
