bb0840b4c746f9b46281aa6da271d8575a1ea2f2
adhabale-incomm <adhabale@InComm.com>
2021-04-05 15:26:48 -0400

    Update R__Repeatable_data_DDL.sql

--- Flyway/R__Repeatable_data_DDL.sql
+++ Flyway/R__Repeatable_data_DDL.sql
@@ -1,4 +1,4 @@
-Create table test_re(id number(8),name varchar(50));
+--Create table test_re(id number(8),name varchar(50));
 
 ALTER TABLE test_re 
 ADD birth_date DATE ;
@@ -11,6 +11,8 @@ ADD city varchar(50) ;
 
 ALTER TABLE test_re 
 ADD citystate varchar(50) ;
+
+
 
 
 
