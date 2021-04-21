df65421fe803c2d933f94c0ec721a4e00bc33a1a
adhabale-incomm <adhabale@InComm.com>
2021-04-21 14:31:46 -0400

    Update R__Repeatable_data_DDL.sql

--- Flyway/R__Repeatable_data_DDL.sql
+++ Flyway/R__Repeatable_data_DDL.sql
@@ -1,14 +1,15 @@
-declare n number(1):=0;
+--declare n number(1):=0;
 
-Begin
-select count(*) into n from all_tables where table_name='TEST_RE';
+--Begin
+--select count(*) into n from all_tables where table_name='TEST_RE';
 
-if (n=0) then
+--if (n=0) then
 
-Execute immediate 'Create table test_re(id number(8),name varchar(50))';
- end if ;
-END;
+----Execute immediate 'Create table test_re(id number(8),name varchar(50))';
+---- end if ;
+--END;
 
+Create table test_re(id number(8),name varchar(50))
 
 ALTER TABLE test_re 
 ADD birth_date DATE ;
